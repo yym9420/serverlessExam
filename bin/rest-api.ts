06d8fd0ed5d803e5fc9840e16cf821d54c58bd5b
Yiming Yu <2693604213@qq.com>
2024-03-22 10:50:00 +0000

    Added new endpoint

--- bin/rest-api.ts
+++ bin/rest-api.ts
@@ -2,8 +2,39 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { RestAPIStack } from '../lib/rest-api-stack';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import { Stack, StackProps } from '@aws-cdk/core';
+
+export class NewEndpointStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+    // Define a Lambda function
+    const handler = new lambda.Function(this, 'CrewHandler', {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      handler: 'index.handler',
+      code: lambda.Code.fromAsset('lambda'), // assuming your Lambda code is in a folder named 'lambda'
+    });
+
+    // Define an API Gateway REST API
+    const api = new apigateway.RestApi(this, 'CrewAPI', {
+      restApiName: 'Crew Service',
+      description: 'This service serves crew information for movies.',
+    });
+
+  
+    const getCrewIntegration = new apigateway.LambdaIntegration(handler);
+    const crew = api.root.addResource('crew');
+    const role = crew.addResource('{role}');
+    const movies = role.addResource('movies');
+    const movieId = movies.addResource('{movieId}');
+    movieId.addMethod('GET', new apigateway.LambdaIntegration(handler));
+  }
+}
+
 
 const app = new cdk.App();
+new NewEndpointStack(app, 'NewEndpointStack');
 new RestAPIStack(app, 'RestAPIStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
